6027bb23df458534ee7bdc4432d630163d804dfe
mk1121 <marufm.3666.com>
2021-07-30 11:48:02 +0600

    error with git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import {useEffect, useState} from 'react';
-import './App.css';
-import User from './component/User/User';
+import { useEffect, useState } from "react";
+import "./App.css";
+import User from "./component/User/User";
 function App() {
+  const [user, setUser] = useState([]);
 
-const [user,setUser] = useState([]);
-	
-	useEffect(() =>{
-	fetch("https://randomuser.me/api/?results=15")	
-	.then(res => res.json())
-	.then(data => {setUser(data)
-	console.log(data)} 
-	)
-	},[])
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=15")
+      .then((res) => res.json())
+      .then((da) => {
+        setUser(da.results);
+      });
+  }, []);
   return (
     <div className="App">
-	  {user.map((usr) => 
-		<User
-		data={usr.results}
-		  ></User>
-	  )}
-	  </div>
+      {user.map((usr) => (
+        <User data={usr}></User>
+      ))}
+    </div>
   );
 }
 
